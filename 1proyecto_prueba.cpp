1823009cf393dea6a54571ee7d364e11eb8c7597
Jayjay <jasstif09@gmail.com>
2026-08-17 18:56:19 -0600

    Create 1proyecto_prueba.cpp
    progando

---
+++ 1proyecto_prueba.cpp
@@ -0,0 +1,50 @@
+
+#include <iostream>
+int main(){
+
+int i =0;
+
+
+do {
+		std::cout<<"\n        		 << MINI GIT >>\n";
+		std::cout<<"		<<1. Inicializar repositorio.>>\n";
+		std::cout<<"		<<2. Ver informacion del repositorio>>\n";
+		std::cout<<	"		<< 3.Simular Cambios.>>\n";
+		std::cout<<"		<<4. Crear Commit>>\n";
+		std::cout<<"		<<5. Ver Ultimo Commit>>\n";
+		std::cout<<"		<<6. Ver estado del repositorio>>\n";
+		std::cout<<"		<<7. Reiniciar repositorio>>\n";
+		std::cout<<"		<<8 .salir>>\n";
+		std::cin>> i;
+		
+		switch(i){
+			
+			case 1:
+				
+				
+				break;
+				system("cls");
+			case 2:
+			
+				break;
+				system("cls");
+			case 3:
+				system("cls");
+			
+				break;
+				system("cls");
+			case 4:
+				system("cls");
+			
+				break;
+				system("cls");
+			default:
+				std::cout<<"opcion no valida ingrese otra: ";
+				break;
+				system("cls");
+		}
+
+	}
+	while (i!=8);
+		return 0;
+}
